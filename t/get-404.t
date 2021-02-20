cosimo/perl6-lwp-simple

throw exception on 4XX/5XX response

t/get-404.t
@@ -1,0 +1,13 @@
+use v6;
+use Test;
+
+use LWP::Simple;
+
+plan 1;
+
+# these tests will fail if this url stops returning 404 response
+throws-like {
+    LWP::Simple.get('http://www.perl6.org/404');
+},
+X::LWP::Simple::Response,
+status => rx:i:s/404 Not Found/;

raku-community-modules/LWP-Simple

t/get-404.t
@@ -1,0 +1,13 @@
+use v6;
+use Test;
+
+use LWP::Simple;
+
+plan 1;
+
+# these tests will fail if this url stops returning 404 response
+throws-like {
+    LWP::Simple.get('http://www.example.com/404');
+},
+X::LWP::Simple::Response,
+status => rx:i:s/404 Not Found/;